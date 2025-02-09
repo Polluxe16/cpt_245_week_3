dc1173b4297376c3c4c6c0638c5b0a819dc916a8
Polluxe <Polluxe16@outlook.com>
2025-02-09 12:38:49 -0500

    Update assignment_2.java

--- assignment_2.java
+++ assignment_2.java
@@ -14,9 +14,9 @@
 public class assignment_2 {
 
     public static void main(String[] args) {
-
-        for(int i = 0; i < 10; i--) {
-            System.out.println("The value if i is: " + i);
+                                      //The problem was that the number kept getting subtracted from, so the number never became higher than 10 to stop the loop.
+        for(int i = 0; i < 10; i++) { //The solution is to make the variable have a number added to it each time rather than subtracted by using ++ instead of --.
+            System.out.println("The value if i is: " + i); //It will count to 9, then show the completion message.
         }
 
         System.out.println("Program has completed successfully!");
